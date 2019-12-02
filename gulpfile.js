d9e3652574a77e9845475fbe9bd4eaa343dd1b02
MohamedElsayed24 <mohamedelsayedhussein0@gmail.com>
2019-12-02 11:42:33 +0200

    j

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require ('gulp'),
+    pug = require ('gulp-pug'),
+    sass = require ('gulp-sass'),
+    plumber  = require ('gulp-plumber'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concat  = require ('gulp-concat'),
+    sourcemaps  = require ('gulp-sourcemaps'),
+    uglify  = require ('gulp-uglify'),
+    imagemin = require ('gulp-imagemin'),
+    watch = require ('gulp-watch');
+
+gulp.task('pug', () => {
+    return gulp.src('./src/pug/*.pug')
+        .pipe(plumber())
+        .pipe(pug({pretty:true}))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('sass', () => {
+    gulp.src('./src/sass/**/*.scss')
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle:'compressed'}))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(concat('style.min.css'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./dist/css'));
+    });
+
+gulp.task('js', () => {
+    return gulp.src('./src/js/partials/*.js')
+        .pipe(plumber())
+        .pipe(concat('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('imagemin', () => {
+    return gulp.src('./src/img/**/*.*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/img'));
+});
+
+gulp.task('watch', () => {
+    gulp.watch('./src/pug/**/*.pug', ['pug']);
+    gulp.watch('./src/sass/**/*.scss', ['sass']);
+    gulp.watch('./src/js/partials/*.js', ['js']);
+    gulp.watch('./src/img/**/*.*', ['imagemin']);
+});
+
+gulp.task('default', ['watch']);
